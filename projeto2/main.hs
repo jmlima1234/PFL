8e7313b83086f72dacebeccc09c86acf85e465ca
Tomás Martins <up202108776@fe.up.pt>
2023-12-27 15:02:28 +0000

    run working

--- projeto2/main.hs
+++ projeto2/main.hs
@@ -37,8 +37,40 @@ createEmptyState = [] -- TODO, Uncomment the function signature after defining S
 state2Str :: State -> String
 state2Str state = intercalate "," (sortOn id (map (\(var, val) -> var ++ "=" ++ show val) state))
 
--- run :: (Code, Stack, State) -> (Code, Stack, State)
-run = undefined -- TODO
+run :: (Code, Stack, State) -> (Code, Stack, State)
+run ([], stack, state) = ([], stack, state)
+run ((Push n):code, stack, state) = run (code, (Push n):stack, state)
+run (Add:code, (Push n1):(Push n2):stack, state) = run (code, (Push (n1 + n2)):stack, state)
+run (Mult:code, (Push n1):(Push n2):stack, state) = run (code, (Push (n1 * n2)):stack, state)
+run (Sub:code, (Push n1):(Push n2):stack, state) = run (code, (Push (n1 - n2)):stack, state)
+run (Tru:code, stack, state) = run (code, (Tru):stack, state)
+run (Fals:code, stack, state) = run (code, (Fals):stack, state)
+run (Equ:code, (Push n1):(Push n2):stack, state) = run (code, (if n1 == n2 then Tru else Fals):stack, state)
+run (Le:code, (Push n1):(Push n2):stack, state) = run (code, (if n1 <= n2 then Tru else Fals):stack, state)
+run (And:code, (Tru):(Tru):stack, state) = run (code, (Tru):stack, state)
+run (And:code, (Fals):(Tru):stack, state) = run (code, (Fals):stack, state)
+run (And:code, (Tru):(Fals):stack, state) = run (code, (Fals):stack, state)
+run (And:code, (Fals):(Fals):stack, state) = run (code, (Fals):stack, state)
+run (And:code, stack, state) = error "Run-time error"
+run (Neg:code, (Tru):stack, state) = run (code, (Fals):stack, state)
+run (Neg:code, (Fals):stack, state) = run (code, (Tru):stack, state)
+run ((Fetch var):code, stack, state) = run (code, (Push (fetch var state)):stack, state)
+run ((Store var):code, (Push n):stack, state) = run (code, stack, (store var n state))
+run (Noop:code, stack, state) = run (code, stack, state)
+run ((Branch code1 code2):code, (Tru):stack, state) = run (code1 ++ code, stack, state)
+run ((Branch code1 code2):code, (Fals):stack, state) = run (code2 ++ code, stack, state)
+run ((Loop code1 code2):code, (Tru):stack, state) = run (code1 ++ [Loop code1 code2] ++ code, stack, state)
+run ((Loop code1 code2):code, (Fals):stack, state) = run (code, stack, state)
+
+fetch :: String -> State -> Integer
+fetch var state = 
+  case lookup var state of
+    Nothing -> error ("Variable " ++ var ++ " not found")
+    Just n  -> n
+
+store :: String -> Integer -> State -> State
+store var n state = (var, n) : filter ((/= var) . fst) state
+
 
 -- Part 2
 
