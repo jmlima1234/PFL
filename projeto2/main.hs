ece6c9876ab6dc994dcc896dd0d95f4af4522eac
João Lima <up202108891@up.pt>
2023-12-31 17:50:43 +0000

    added main

--- projeto2/main.hs
+++ projeto2/main.hs
@@ -246,3 +246,52 @@ parseAddOrSubMultOrAexpOrParent tokens =
 --      case parseStm tokens of
 --        Just (stm, restTokens) -> stm : parseProgram restTokens
 --        _ -> []
+
+-- Examples:
+main :: IO ()
+main = do
+    ---------------------------------------------------------------------------------------
+    -- PART 1 TESTS
+    print $ testAssembler [Push 10,Push 4,Push 3,Sub,Mult] == ("-10","")
+    print $ testAssembler [Fals,Push 3,Tru,Store "var",Store "a", Store "someVar"] == ("","a=3,someVar=False,var=True")
+    print $ testAssembler [Fals,Store "var",Fetch "var"] == ("False","var=False")
+    print $ testAssembler [Push (-20),Tru,Fals] == ("False,True,-20","")
+    print $ testAssembler [Push (-20),Tru,Tru,Neg] == ("False,True,-20","")
+    print $ testAssembler [Push (-20),Tru,Tru,Neg,Equ] == ("False,-20","")
+    print $ testAssembler [Push (-20),Push (-21), Le] == ("True","")
+    print $ testAssembler [Push 5,Store "x",Push 1,Fetch "x",Sub,Store "x"] == ("","x=4")
+    print $ testAssembler [Push 10,Store "i",Push 1,Store "fact",Loop [Push 1,Fetch "i",Equ,Neg] [Fetch "i",Fetch "fact",Mult,Store "fact",Push 1,Fetch "i",Sub,Store "i"]] == ("","fact=3628800,i=1")
+    -- If you test:
+    -- testAssembler [Push 1,Push 2,And]
+    -- You should get an exception with the string: "Run-time error"
+    -- If you test:
+    -- testAssembler [Tru,Tru,Store "y", Fetch "x",Tru]
+    -- You should get an exception with the string: "Run-time error"
+    ---------------------------------------------------------------------------------------
+    -- PART 2 TESTS
+    print $ testParser "x := 5; x := x - 1;" == ("","x=4")
+    print $ testParser "x := 0 - 2;" == ("","x=-2")
+    print $ testParser "if (not True and 2 <= 5 = 3 == 4) then x :=1; else y := 2;" == ("","y=2")
+    print $ testParser "x := 42; if x <= 43 then x := 1; else (x := 33; x := x+1;);" == ("","x=1")
+    print $ testParser "x := 42; if x <= 43 then x := 1; else x := 33; x := x+1;" == ("","x=2")
+    print $ testParser "x := 42; if x <= 43 then x := 1; else x := 33; x := x+1; z := x+x;" == ("","x=2,z=4")
+    print $ testParser "x := 44; if x <= 43 then x := 1; else (x := 33; x := x+1;); y := x*2;" == ("","x=34,y=68")
+    print $ testParser "x := 42; if x <= 43 then (x := 33; x := x+1;) else x := 1;" == ("","x=34")
+    print $ testParser "if (1 == 0+1 = 2+1 == 3) then x := 1; else x := 2;" == ("","x=1")
+    print $ testParser "if (1 + 1 == 2 = 1 + 2 == 3) then x := 1; else x := 2;" == ("","x=1")
+    print $ testParser "if (1 == 0+1 = (2+1 == 4)) then x := 1; else x := 2;" == ("","x=2")
+    print $ testParser "x := 2; y := (x - 3)*(4 + 2*3); z := x +x*(2);" == ("","x=2,y=-10,z=6")
+    print $ testParser "i := 10; fact := 1; while (not(i == 1)) do (fact := fact * i; i := i - 1;);" == ("","fact=3628800,i=1")
+    print $ testParser "x := 5; x := x - 1;" == ("","x=4")
+    print $ testParser "x := 0 - 2;" == ("","x=-2")
+    print $ testParser "if (not True and 2 <= 5 = 3 == 4) then x :=1; else y := 2;" == ("","y=2")
+    print $ testParser "x := 42; if x <= 43 then x := 1; else (x := 33; x := x+1;);" == ("","x=1")
+    print $ testParser "x := 42; if x <= 43 then x := 1; else x := 33; x := x+1;" == ("","x=2")
+    print $ testParser "x := 42; if x <= 43 then x := 1; else x := 33; x := x+1; z := x+x;" == ("","x=2,z=4")
+    print $ testParser "x := 44; if x <= 43 then x := 1; else (x := 33; x := x+1;); y := x*2;" == ("","x=34,y=68")
+    print $ testParser "x := 42; if x <= 43 then (x := 33; x := x+1;) else x := 1;" == ("","x=34")
+    print $ testParser "if (1 == 0+1 = 2+1 == 3) then x := 1; else x := 2;" == ("","x=1")
+    print $ testParser "if (1 == 0+1 = (2+1 == 4)) then x := 1; else x := 2;" == ("","x=2")
+    print $ testParser "x := 2; y := (x - 3)*(4 + 2*3); z := x +x*(2);" == ("","x=2,y=-10,z=6")
+    print $ testParser "i := 10; fact := 1; while (not(i == 1)) do (fact := fact * i; i := i - 1;);" == ("","fact=3628800,i=1")
+    ---------------------------------------------------------------------------------------
